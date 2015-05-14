c1e74ff9b2fe51c88672b747a54abeb5ae504624
Jimmy Yen <zeng.tw@gmail.com>
2015-05-15 05:26:57 +0800

    gentoo virtualbox script use 4.3.26

--- scripts/gentoo/scripts/virtualbox.sh
+++ scripts/gentoo/scripts/virtualbox.sh
@@ -5,9 +5,9 @@ emerge "=virtual/linux-sources-1" --autounmask-write
 etc-update --automode -5
 emerge "=virtual/linux-sources-1"
 
-emerge ">=app-emulation/virtualbox-guest-additions-4.3-26" --autounmask-write
+emerge ">=app-emulation/virtualbox-guest-additions-4.3.26" --autounmask-write
 etc-update --automode -5
-emerge ">=app-emulation/virtualbox-guest-additions-4.3-26"
+emerge ">=app-emulation/virtualbox-guest-additions-4.3.26"
 
 rc-update add virtualbox-guest-additions default
 EOF
