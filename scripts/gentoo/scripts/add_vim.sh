f940c71920b9552024f027a6cb8724f05dcc6b53
Jimmy Yen <zeng.tw@gmail.com>
2015-05-15 23:22:04 +0800

    will also uninstall nano editor

--- scripts/gentoo/scripts/add_vim.sh
+++ scripts/gentoo/scripts/add_vim.sh
@@ -1,10 +1,11 @@
 #!/bin/env bash
 
-# add minimal use flags 
+# install vim and vim-core use minimal flags, and remove nano editor
 chroot /mnt/gentoo /bin/bash <<'EOF'
 echo "app-editors/vim-core minimal" > /etc/portage/package.use/vim
 echo "app-editors/vim minimal" >> /etc/portage/package.use/vim
 emerge app-editors/vim-core app-editors/vim
 eselect editor set vi
+emerge -c app-editors/nano
 EOF
 
